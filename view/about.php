3f8864a82fd0dad7bed22e687e680c006e1fe695
joshuaevern <everncamfed@gmail.com>
2021-11-14 17:04:14 +0000

    updated about

---
+++ view/about.php
@@ -0,0 +1,388 @@
+<?php
+
+	include_once "../services/header.html";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>About Us</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="Vide" />
+<meta name="keywords" content="Big store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- js -->
+   <script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+<!--- start-rate---->
+<script src="js/jstarbox.js"></script>
+	<link rel="stylesheet" href="css/jstarbox.css" type="text/css" media="screen" charset="utf-8" />
+		<script type="text/javascript">
+			jQuery(function() {
+			jQuery('.starbox').each(function() {
+				var starbox = jQuery(this);
+					starbox.starbox({
+					average: starbox.attr('data-start-value'),
+					changeable: starbox.hasClass('unchangeable') ? false : starbox.hasClass('clickonce') ? 'once' : true,
+					ghosting: starbox.hasClass('ghosting'),
+					autoUpdateAverage: starbox.hasClass('autoupdate'),
+					buttons: starbox.hasClass('smooth') ? false : starbox.attr('data-button-count') || 5,
+					stars: starbox.attr('data-star-count') || 5
+					}).bind('starbox-value-changed', function(event, value) {
+					if(starbox.hasClass('random')) {
+					var val = Math.random();
+					starbox.next().text(' '+val);
+					return val;
+					} 
+				})
+			});
+		});
+		</script>
+<!---//End-rate---->
+
+</head>
+<body>
+  <!---->
+    <!--banner-->
+<div class="banner-top">
+	<div class="container">
+		<h3 >About</h3>
+		<h4><a href="index.html">Home</a><label>/</label>About</h4>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+
+<!-- faqs -->
+	<div class="about-w3 ">
+
+			
+			<!--about-->
+			<div class="container">
+		<div  class="about">
+	<div class="spec ">
+				<h3>About</h3>
+					<div class="ser-t">
+						<b></b>
+						<span><i></i></span>
+						<b class="line"></b>
+					</div>
+			</div>
+			
+			<div class="col-md-4 about-right">
+			<img class="img-responsive" src="images/ab.jpg" alt="">
+			</div>
+			<div class="col-md-4 about-left">
+				<p>In the northern part of Ghana, specifically the Upper East Region, migration of young adults, from the north to the south in search of jobs to support their families, continue their education or to earn a living. They end up working as head-porters (Kayayei) in the major urban cities in Ghana. The pilgrimage is perilous ,and life when they finally arrive in the south is harsh. These women live and work on the streets where they become vulnerable to physical and sexual abuse as well as robbery. 
+  </p>
+			</div>
+			<div class="col-md-4 about-right">
+			<img class="img-responsive" src="images/ab1.jpg" alt="">
+			</div>
+			
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!--//about-->
+	
+	<!--work-experience-->
+	<div  class="work">
+		<div class="container">
+			<div class="spec spec-w3ls">
+				<h3>Our Journey</h3>
+					<div class="ser-t">
+						<b></b>
+						<span><i></i></span>
+						<b class="line"></b>
+					</div>
+			</div>
+			<!-- <div class="work-info"> 
+				<div class="col-md-6 work-left"> 
+					<div class=" work-w3 "> 
+						<h5> May 2012</h5>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="col-md-6 work-right"> 
+					<div class=" work-w31"> 
+						<h5> November 2012</h5>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="clearfix"> </div>
+				<span> 2012</span>
+			</div>
+			<div class="work-info"> 
+				<div class="col-md-6 work-left"> 
+					<div class=" work-w3 "> 
+						<h5> June 2013</h5>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="col-md-6 work-right"> 
+					<div class=" work-w31"> 
+						<h5>December 2013</h5>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="clearfix"> </div>
+				<span> 2013</span>
+			</div> -->
+			<div class="work-info"> 
+				<div class="col-md-6 work-left"> 
+					<div class=" work-w3 "> 
+						<h5> April 2014</h5>
+						<p>To combat the spread of Covid-19, we are producing reusable nose masks. You can get 5 of these quality nose masks at 20 cedis. </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="col-md-6 work-right"> 
+					<div class=" work-w31"> 
+						<h5> August 2014</h5>
+						<p>Loozeele Initiative is happy to announce our partnership with @sanhwe_scents. With every perfume, oil or any product that you purchase from them, 5% goes to support a kayayei girl in Loozeele. </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="clearfix"> </div>
+				<span> 2014</span>
+			</div>
+			<div class="work-info"> 
+				<div class="col-md-6 work-left"> 
+					<div class=" work-w3 "> 
+						<h5> February 2015</h5>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="col-md-6 work-right"> 
+					<div class=" work-w31"> 
+						<h5> July 2015</h5>
+						<p>We had over 17 girls under our smock fabric production.  </p>
+					</div>
+					<label></label>
+				</div>
+				<div class="clearfix"> </div>
+				<span> 2015</span>
+				<span class="last"> 2016</span>
+			</div>
+		</div>
+	</div>
+	<!--//work-experience-->
+<!--advantages--> 
+<div class="container">
+	<div class="advantages">
+			<div class="col-md-6 advantages-left ">
+				<h3>Our Advantages</h3>
+				<div class="advn-one">
+						<div class="ad-mian">
+							<div class="ad-left">
+								<p>1</p>
+							</div>
+							<div class="ad-right">
+								<h4><a href="single.html">Hand made products</a></h4>
+								<p>Our products helps in reducing climate changes since we make it from materials that could ave been wasted. </p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="ad-mian">
+							<div class="ad-left">
+								<p>2</p>
+							</div>
+							<div class="ad-right">
+								<h4><a href="single.html">Customer Satisfaction</a></h4>
+								<p>We provide before and after sale customer services to our customers to make sure that they are satisfied with our products </p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="ad-mian">
+							<div class="ad-left">
+								<p>3</p>
+							</div>
+							<div class="ad-right">
+								<h4><a href="single.html">Delivery</a></h4>
+								<p>We deliver within 5 days of purchase </p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+			</div>
+			<div class="col-md-6 advantages-left about-agi">
+				<h3>Our Skills</h3>
+			<div class="advn-two">
+						<h4><a href="single.html"> Proudly made in Ghana</a></h4>
+						<p>We do everything, from weaving to crafting</p>
+						<ul>
+							<li><a href="#"> <i class="fa fa-angle-right" aria-hidden="true"></i>Weaving Skills</a></li>
+							<li><a href="#"> <i class="fa fa-angle-right" aria-hidden="true"></i>Shea Butter making skills</a></li>
+							<li><a href="#"> <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+
+						</ul>
+			</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		</div>
+	<!--advantages--> 
+
+	</div>
+	<!-- // Terms of use -->
+
+<!--footer-->
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 footer-grid">
+			<h3>About Us</h3>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+	</ul>
+			<p>Loozeele Initiative is an educational program targeted at the youth in the northern parts of Ghana. It aims to empower young people to identify entrepreneurial opportunities and sent more kayayei back to school and provide them with skills whiles reducing</p>
+		</div>
+		<div class="col-md-3 footer-grid ">
+			<h3>Menu</h3>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="kitchen.html">Kitchen</a></li>
+				<li><a href="care.html">Personal Care</a></li>
+				<li><a href="hold.html">Household</a></li>						 
+				<li><a href="codes.html">Short Codes</a></li> 
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grid ">
+			<h3>Customer Services</h3>
+			<ul>
+				<li><a href="shipping.html">Shipping</a></li>
+				<li><a href="terms.html">Terms & Conditions</a></li>
+				<li><a href="faqs.html">Faqs</a></li>
+				<li><a href="contact.html">Contact</a></li>
+				<li><a href="offer.html">Online Shopping</a></li>						 
+				 
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grid">
+			<h3>My Account</h3>
+			<ul>
+				<li><a href="login.html">Login</a></li>
+				<li><a href="register.html">Register</a></li>
+				<li><a href="wishlist.html">Wishlist</a></li>
+				
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+			<div class="footer-bottom">
+				<h2 ><a href="index.html"><b>T<br>H<br>E</b>Big Store<span>The Best Supermarket</span></a></h2>
+				<p class="fo-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+				<ul class="social-fo">
+					<li><a href="#" class=" face"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#" class=" twi"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="#" class=" pin"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+					<li><a href="#" class=" dri"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+				</ul>
+				<div class=" address">
+					<div class="col-md-4 fo-grid1">
+							<p><i class="fa fa-home" aria-hidden="true"></i>12K Street , 45 Building Road Canada.</p>
+					</div>
+					<div class="col-md-4 fo-grid1">
+							<p><i class="fa fa-phone" aria-hidden="true"></i>+1234 758 839 , +1273 748 730</p>	
+					</div>
+					<div class="col-md-4 fo-grid1">
+						<p><a href="mailto:info@example.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@example1.com</a></p>
+					</div>
+					<div class="clearfix"></div>
+					
+					</div>
+			</div>
+		<div class="copy-right">
+			<p> &copy; 2016 Big store. All Rights Reserved | Design by  <a href="http://w3layouts.com/"> W3layouts</a></p>
+		</div>
+	</div>
+</div>
+<!-- //footer-->
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+<!-- for bootstrap working -->
+		<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<script type='text/javascript' src="js/jquery.mycart.js"></script>
+  <script type="text/javascript">
+  $(function () {
+
+    var goToCartIcon = function($addTocartBtn){
+      var $cartIcon = $(".my-cart-icon");
+      var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+      $addTocartBtn.prepend($image);
+      var position = $cartIcon.position();
+      $image.animate({
+        top: position.top,
+        left: position.left
+      }, 500 , "linear", function() {
+        $image.remove();
+      });
+    }
+
+    $('.my-cart-btn').myCart({
+      classCartIcon: 'my-cart-icon',
+      classCartBadge: 'my-cart-badge',
+      affixCartIcon: true,
+      checkoutCart: function(products) {
+        $.each(products, function(){
+          console.log(this);
+        });
+      },
+      clickOnAddToCart: function($addTocart){
+        goToCartIcon($addTocart);
+      },
+      getDiscountPrice: function(products) {
+        var total = 0;
+        $.each(products, function(){
+          total += this.quantity * this.price;
+        });
+        return total * 1;
+      }
+    });
+
+  });
+  </script>
+
+</body>
+</html>
